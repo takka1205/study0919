c085fd5b00d42b96de212d23b00423eaa63f43ba
石川貴洋 <>
2024-09-19 14:29:07 +0900

    Describe your changes

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -1,25 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function main() {
-  try {
-    await prisma.$connect();
-  } catch {
-    return Error("DB接続に失敗しました");
-  }
-}
-
-export const POST = async (req: Request) => {
+export async function POST(req: NextRequest) {
   const { name, email } = await req.json();
+
   try {
-    await main();
+    // Prisma Client での処理
     const posts = await prisma.formSubmission.create({ data: { name, email } });
-    return NextResponse.json({ message: "成功", posts }, { status: 300 });
+    return NextResponse.json({ message: "成功", posts }, { status: 200 }); // 成功時は 200 ステータスコード
   } catch (err) {
     return NextResponse.json({ message: "失敗", err }, { status: 500 });
   } finally {
-    await prisma.$disconnect();
+    await prisma.$disconnect(); // データベース接続のクリーンアップ
   }
-};
+}
